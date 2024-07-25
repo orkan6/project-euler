7c93c27213b0c80015a60f2c971926484d988a1c
orkan6 <158033955+orkan6@users.noreply.github.com>
2024-07-26 01:19:48 +0300

    Create multiple_3_5.py

---
+++ multiple_3_5.py
@@ -0,0 +1,16 @@
+#Find the sum of all the multiples of or below .
+
+numb3 = 0
+numb5 = 0
+numb15 = 0
+
+for i in range(0,1000,3):
+    numb3 += i
+
+for j in range(0,1000,5):
+    numb5 += j
+
+for k in range(0,1000,15):
+    numb15 += k
+
+print((numb3+numb5)-numb15)
